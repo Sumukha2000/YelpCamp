66653fe1a8a00227fd6c522391a9b154a3619e44
Sumukha2000 <57064739+Sumukha2000@users.noreply.github.com>
2020-03-27 23:53:51 +0530

    init setup

---
+++ routes/campgrounds.js
@@ -0,0 +1,105 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground")
+var middleware=require("../middleware/index.js")
+router.get("/", function (req, res) {//get all cg from db
+
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
+        }
+    })
+
+    //res.render("campgrounds",{campgrounds:campgrounds});
+});
+//CREATE
+router.post("/",middleware.isLoggedIn, function (req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = { name: name,price: price, image: image, description: description, author: author };
+    //create a new cg and save to db
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+
+            res.redirect("/campgrounds");
+        }
+    });
+
+
+
+});
+//NEW 
+router.get("/new", middleware.isLoggedIn, function (req, res) {
+    res.render("campgrounds/new");
+});
+router.get("/:id", function (req, res) {
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds/show", { campground: foundCampground });
+        }
+    });
+
+    //render show template
+
+
+});
+//EDIT CAMPGROUND ROUTE
+
+router.get("/:id/edit",middleware.checkCampgroundOwnership, function(req, res) {
+   
+        Campground.findById(req.params.id, function(err, foundCampground) {
+
+                    res.render("campgrounds/edit", { campground: foundCampground });
+        });
+       
+    });
+
+
+
+//UPDATE
+
+router.put("/:id", middleware.checkCampgroundOwnership,function (req, res) {
+    //find and update
+    var data = {}
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, updatedCampground) {
+        if (err) {
+            res.redirect("/campgrounds")
+        }
+        else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+
+
+})
+
+//DESTROY CAMPGROUND ROUTE
+router.delete("/:id",middleware.checkCampgroundOwnership ,function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        }
+        else {
+            res.redirect("/campgrounds");
+        }
+    })
+})
+
+
+
+
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,66 @@
+var express=require("express");
+var router=express.Router();
+var passport=require("passport");
+var User=require("../models/user")
+
+//root route
+router.get("/",function(req,res)
+{
+res.render("landing");
+   // res.send("this will be the landing page")
+});
+
+//================
+//AUTH ROUTES
+//================
+//show reg form
+router.get("/register",function(req,res)
+{
+    res.render("register")
+})
+//handle sign up logic
+router.post("/register",function(req,res)
+{
+    var newUser=new User({username: req.body.username});
+    User.register(newUser,req.body.password,function(err,user)
+    {
+        if(err){
+            req.flash("error", err.message);
+            return res.redirect("/register");
+          }
+        passport.authenticate("local")(req,res,function()
+        {
+            req.flash("success","Welcome to YelpCamp!"+user.username);
+            res.redirect("/campgrounds");
+        })
+    })
+})
+//show login
+router.get("/login",function(req,res)
+{
+    res.render("login")
+});
+//handling login logic
+router.post("/login",passport.authenticate("local",
+{successRedirect:"/campgrounds",
+failureRedirect:"/login"
+}),
+    
+    function(req,res){
+   
+});
+//logic logout route
+router.get("/logout",function(req,res)
+{
+    req.logout();
+    req.flash("success","Logged you out!");
+    res.redirect("/campgrounds")
+});
+//MIDDLEWARE
+function isLoggedIn(req,res,next)
+{
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}module.exports=router;
